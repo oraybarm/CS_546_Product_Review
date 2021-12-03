4775a1a4c77645bf74967419f3c5f8fa5c97ab6b
Sacheth Shetty <31033563+Sach24eth@users.noreply.github.com>
2021-12-02 23:38:13 -0500

    Pushing getAllProducts (#18)

--- server/routes/productRoute.js
+++ server/routes/productRoute.js
@@ -3,32 +3,70 @@ const router = express.Router();
 const productData = require("../data/products");
 const session = require("express-session");
 
+// router.get("/", async (req, res) => {
+//   try {
+//     let prodList = await productData.getAllProducts();
+//     res.status(200).render("homePage/homePage", {
+//       products: prodList,
+//     });
+//   } catch (e) {
+//     console.log(e);
+//     res.status(404).send(e);
+//   }
+// });
 router.post("/search", async (req, res) => {
-    const body = req.body;
-    let option = document.getElementById("searchSelect");
-    let searchTerm = body.search;
-    if (!searchTerm || searchTerm.trim() == 0) {
-        return "Error: Search term blank";
-    } else {
-        searchTerm = searchTerm.toLowerCase();
-        if (option.value === "By Name") {
-            try {
-                let search_List = await products.getProductByProductName(
-                    body.search
-                );
-                res.status(200).render("homePage/homePage", {
-                    products: search_List,
-                });
-            } catch (e) {
-                return res.status(500).message(e.message);
-            }
-        }
-        if (option.value === "By Tag") {
-            try {
-                let search_List = await products.getProductbyTag(body.search);
-            } catch (e) {
-                return res.status(500).send();
-            }
-        }
+  const body = req.body;
+  //console.log("body", body);
+
+  //console.log(option);
+  let searchTerm = body.searchInput;
+  let searchValue = body.searchSelect;
+  //console.log(searchTerm);
+  //console.log(searchValue);
+  if (!searchTerm || searchTerm.trim().length == 0) {
+    return "Error: Search term blank";
+  } else {
+    searchTerm = searchTerm.toLowerCase();
+    console.log(searchTerm);
+    //console.log(option.value);
+    if (searchValue === "name") {
+      try {
+        let search_List = await productData.getProductByProductName(searchTerm);
+        //return only the json
+
+        res.status(200).render("searchPage/searchPage", {
+          products: search_List,
+        });
+      } catch (e) {
+        return res.status(404).render("errorPage/noSearch");
+      }
     }
+    if (searchValue === "tag") {
+      try {
+        let search_List = await productData.getProductbyTag(searchTerm);
+        res.status(200).render("searchPage/searchPage", {
+          products: search_List,
+        });
+      } catch (e) {
+        return res.status(404).render("errorPage/noSearch");
+      }
+    }
+  }
 });
+
+router.post("/addProduct", async (req, res) => {
+  if (!req.session.AuthCookie) {
+    res.status(401).redirect("/");
+  } else {
+    //check what all is required after making the front end form
+    const {
+      product_Name,
+      description,
+      website_Url,
+      // logo,
+      tags,
+      developer,
+    } = req.body;
+  }
+});
+module.exports = router;
