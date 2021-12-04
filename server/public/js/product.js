f954ee95b47a70623aeb94740a699dcbc5459025
oraybarm <ganesh.raybarman@gmail.com>
2021-12-04 16:17:01 -0500

    added clientside js file for product

---
+++ server/public/js/product.js
@@ -0,0 +1,35 @@
+(function ($) 
+{
+  let likeIcon = document.querySelector("#icon"),
+    count = document.querySelector("#count");
+  var url = window.location.href;
+  var id = url.split("/")[4];
+  var liked = $.find("#likedProduct");
+  var flag = false;
+  if (liked.length) {
+    likeIcon.innerHTML = `<i class="fas fa-thumbs-up"></i>`;
+    flag = true;
+  }
+  
+  $(".like__btn").click(function (event) {
+    //event.preventDefault();
+
+    if (!flag) {
+      flag = true;
+      likeIcon.innerHTML = `<i class="fas fa-thumbs-up"></i>`;
+      count.textContent++;
+    } 
+    else {
+      flag = false;
+      likeIcon.innerHTML = `<i class="far fa-thumbs-up"></i>`;
+      count.textContent--;
+    }
+
+    $.ajax({
+      type: "POST",
+      url: "/product/updateLike",
+      data: { productId: id, liked: flag },
+    });
+  });
+})
+(window.jQuery);
