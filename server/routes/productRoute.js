2fcdcf51dc1811df3ac454fcbb2b45eb84b0db8a
dsl233 <3137912050@qq.com>
2021-12-04 19:10:03 -0500

    add product without validation

--- server/routes/productRoute.js
+++ server/routes/productRoute.js
@@ -4,6 +4,7 @@ const productData = require("../data/products");
 const session = require("express-session");
 const xss = require("xss");
 const multer = require("multer");
+const { authMiddleware } = require("../middlewares/auth");
 // router.get("/", async (req, res) => {
 //   try {
 //     let prodList = await productData.getAllProducts();
@@ -83,10 +84,10 @@ router.post("/search", async (req, res) => {
 
 router.post(
   "/addProduct",
-  upload.single("logo"),
+  upload.single("photo"),
   authMiddleware,
   async (req, res) => {
-    if (!req.session.AuthCookie) {
+    if (!req.session.user) {
       res.status(401).redirect("/");
     } else {
       //check what all is required after making the front end form
@@ -99,15 +100,11 @@ router.post(
       tags = xss(tags);
       developer = developer.toLowerCase();
       developer = xss(developer);
-      let logo = req.file?.filename;
-      logo = xss(logo);
-      // console.log(productName);
+      let photo = req.file.filename;
+      photo = xss(photo);
+      console.log(productName);
 
-      // console.log(websiteUrl);
-      // console.log(tags);
-      // //console.log()
-      // console.log(developer);
-      if (req.file && !req.file?.mimetype.includes("image")) {
+      if (req.file && !req.file.mimetype.includes("image")) {
         return res.status(400).json({ error: "Please upload an image" });
       }
       //Checking if input present in the first place
@@ -129,20 +126,13 @@ router.post(
           .status(400)
           .json({ error: "Details provided are not of proper type string" });
       }
-      // Tags array-type check
-      // if (
-      //   !Array.isArray(tags) ||
-      //   tags.length < 1 ||
-      //   tags.forEach((elem) => {
-      //     if (typeof elem !== "string" || elem.trim().length) {
-      //       return res.status(400).json({ error: "Tags should be of type string" });
-      //     }
-      //   })
-      // ) {
-      //   return res
-      //     .status(400)
-      //     .json({ error: "Tag are empty or not of type of array" });
-      // }
+      let tagslist=tags.split(",");
+      let tagarr=[];
+      for(let i=0;i<tagslist.length;i++){
+        let tag={};
+        tag["name"]=tagslist[i];
+        tagarr.push(tag);
+      }
       let re =
         /^(http:\/\/|https:\/\/)?(www.)?([a-zA-Z0-9]+).[a-zA-Z0-9]*.[‌​a-z]{3}\.([a-z]+)?$/gm;
       if (!re.test(websiteUrl)) {
@@ -150,25 +140,26 @@ router.post(
           error: "Website URL provided does not satisfy proper criteria",
         });
       }
-      const delRest = await restaurantCollection.findOne({
-        productName: productName,
-      });
+      // const delRest = await restaurantCollection.findOne({
+      //   productName: productName,
+      // });
       // finally calling the db function to add the product
       try {
         const newProduct = await productData.addProduct(
           productName,
           description,
           websiteUrl,
-          logo,
-          tags,
+          photo,
+          tagarr,
           developer
         );
         console.log(newProduct);
-        return res.status(200).json(newProduct);
+        res.redirect('/');
       } catch (e) {
         return res.status(500).json({ message: `${e}` });
       }
     }
   }
 );
+
 module.exports = router;
